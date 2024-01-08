4e78e21871495a5d3e214299ab8ffdbdcae75c4d
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2024-01-08 06:37:59 -0600

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -3,7 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth
 import { getFirestore, addDoc, collection, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
 
 const firebaseConfig = {
-  // ...
+  apiKey: "AIzaSyBTjVpCHlqhK6xLUd_F5EOZBVc9ElPnDzQ",
+  authDomain: "mibiblioteca12-c8e68.firebaseapp.com",
+  projectId: "mibiblioteca12-c8e68",
+  storageBucket: "mibiblioteca12-c8e68.appspot.com",
+  messagingSenderId: "882606809500",
+  appId: "1:882606809500:web:6d61d7033b45b22230a859"
 };
 
 // Initialize Firebase
