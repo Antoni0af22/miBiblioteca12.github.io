196984fa78652291861462fd6646dbacd8040ad2
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2024-01-08 07:21:43 -0600

    Update borrar.js

--- app/borrar.js
+++ app/borrar.js
@@ -1,25 +1,36 @@
-import { onGetLibros } from './firebase.js';
+import {onGetLibros,deleteLibro} from './firebase.js'
 
 const librosContainer = document.getElementById('libros-container');
 
 window.addEventListener("DOMContentLoaded", async (e) => {
-    if (librosContainer) {
-        onGetLibros((querySnapshot) => {
-            librosContainer.innerHTML = '';
-
-            querySnapshot.forEach((doc) => {
-                const libro = doc.data();
-                librosContainer.innerHTML += `
-                    <div class="card card-body mt-2 border-primary">
-                        <h3 class="h5">${libro.titulo}</h3>
-                        <p>${libro.autor}</p>
-                        <p>${libro.editorial}</p>
-                        <p>${libro.isbn}</p>
-                        <p>${libro.fecha}</p>
-                    </div>`;
-            });
+    onGetLibros((querySnapshot)=>{
+        librosContainer.innerHTML = "";
+        querySnapshot.forEach((doc) => {        
+            const libro = doc.data();
+            librosContainer.innerHTML += `
+            <div class="card card-body mt-2 border-primary">
+            <h3 class="h5">${libro.titulo}</h3>
+            <p>${libro.autor}</p>
+            <p>${libro.editorial}</p>
+            <p>${libro.isbn}</p>
+            <p>${libro.fecha}</p>
+            <div>
+            <button class="btn btn-primary btn-delete" data-id="${doc.id}">
+                🗑 Delete
+            </button>
+            </div>
+            </div>`;
         });
-    } else {
-        console.error("Elemento con ID 'libros-container' no encontrado en el DOM.");
-    }
-});
+
+        const btnsDelete = librosContainer.querySelectorAll(".btn-delete");
+        btnsDelete.forEach((btn) =>
+        btn.addEventListener("click", async ({ target: { dataset } }) => {
+        try {
+            await deleteLibro(dataset.id);
+        } catch (error) {
+            console.log(error);
+        }
+      })
+    );
+    })
+})
