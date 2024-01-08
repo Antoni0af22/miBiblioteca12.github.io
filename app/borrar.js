589a6a1edbb69d234753bd88dca979d352b2984c
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2024-01-08 06:56:21 -0600

    Update borrar.js

--- app/borrar.js
+++ app/borrar.js
@@ -1,22 +1,22 @@
-import {onGetLibros} from './firebase.js'
+import { onGetLibros } from './firebase.js';
 
 const librosContainer = document.getElementById('libros-container');
 
 window.addEventListener("DOMContentLoaded", async (e) => {
-    onGetLibros((querySnapshot)=>{
-        let html = "";
-        querySnapshot.forEach((doc) => {        
-            const libro = doc.data();
-            html += `
-            <div class="card card-body mt-2 border-primary">
-            <h3 class="h5">${libro.titulo}</h3>
-            <p>${libro.autor}</p>
-            <p>${libro.editorial}</p>
-            <p>${libro.isbn}</p>
-            <p>${libro.fecha}</p>
-            <div>`;
-        });
+    onGetLibros((querySnapshot) => {
+        librosContainer.innerHTML = '';
 
-        librosContainer.innerHTML = html;
-    })
-})
+        querySnapshot.forEach((doc) => {
+            const libro = doc.data();
+            librosContainer.innerHTML += `
+                <div class="card card-body mt-2 border-primary">
+                    <h3 class="h5">${libro.titulo}</h3>
+                    <p>${libro.autor}</p>
+                    <p>${libro.editorial}</p>
+                    <p>${libro.isbn}</p>
+                    <p>${libro.fecha}</p>
+                </div>`;
+        });
+    });
+});
+
