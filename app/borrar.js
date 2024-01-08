bc76c5961286f9e54a1ca7bef6574c000840dced
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2024-01-08 07:04:41 -0600

    Update borrar.js

--- app/borrar.js
+++ app/borrar.js
@@ -3,20 +3,23 @@ import { onGetLibros } from './firebase.js';
 const librosContainer = document.getElementById('libros-container');
 
 window.addEventListener("DOMContentLoaded", async (e) => {
-    onGetLibros((querySnapshot) => {
-        librosContainer.innerHTML = '';
+    if (librosContainer) {
+        onGetLibros((querySnapshot) => {
+            librosContainer.innerHTML = '';
 
-        querySnapshot.forEach((doc) => {
-            const libro = doc.data();
-            librosContainer.innerHTML += `
-                <div class="card card-body mt-2 border-primary">
-                    <h3 class="h5">${libro.titulo}</h3>
-                    <p>${libro.autor}</p>
-                    <p>${libro.editorial}</p>
-                    <p>${libro.isbn}</p>
-                    <p>${libro.fecha}</p>
-                </div>`;
+            querySnapshot.forEach((doc) => {
+                const libro = doc.data();
+                librosContainer.innerHTML += `
+                    <div class="card card-body mt-2 border-primary">
+                        <h3 class="h5">${libro.titulo}</h3>
+                        <p>${libro.autor}</p>
+                        <p>${libro.editorial}</p>
+                        <p>${libro.isbn}</p>
+                        <p>${libro.fecha}</p>
+                    </div>`;
+            });
         });
-    });
+    } else {
+        console.error("Elemento con ID 'libros-container' no encontrado en el DOM.");
+    }
 });
-
